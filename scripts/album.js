bdeeeae1956e31ce2107df13e06075fa9971a3af
Lisa Min <lisajmin@gmail.com>
2016-12-17 13:09:10 -0800

    Assignment 21 - Update time format to minutes and seconds: Complete

--- scripts/album.js
+++ scripts/album.js
@@ -23,8 +23,25 @@ var setVolume = function(volume) {
     }
 };
 
+var setCurrentTimeInPlayerBar = function(currentTime) {
+    $('.current-time').text(filterTimeCode(currentTime));
+};
+
+var setTotalTimeInPlayerBar = function(totalTime) {
+    $('.total-time').text(filterTimeCode(totalTime));
+};
+
 var getSongNumberCell = function(number) {
     return $('.song-item-number[data-song-number="' + number + '"]')
+};
+
+var filterTimeCode = function(timeInSeconds) {
+    var minutes = Math.floor(parseFloat(timeInSeconds) / 60);
+    var seconds = Math.floor(parseFloat(timeInSeconds) % 60);
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    return minutes + ":" + seconds;
 };
 
 
@@ -33,7 +50,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         '<tr class="album-view-song-item">'
       + ' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + ' <td class="song-item-title">' + songName + '</td>'
-      + ' <td class="song-item-duration">' + songLength + '</td>'
+      + ' <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
       + '</tr>';
     
     var $row = $(template);
@@ -121,6 +138,7 @@ var updateSeekBarWhileSongPlays = function() {
           var $seekBar = $('.seek-control .seek-bar');
           
           updateSeekPercentage($seekBar, seekBarFillRatio);
+          setCurrentTimeInPlayerBar(this.getTime());
       });
   }  
 };
@@ -249,6 +267,7 @@ var updatePlayerBarSong = function() {
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
     $('.main-controls .play-pause').html(playerBarPauseButton);
+    setTotalTimeInPlayerBar(currentSongFromAlbum.duration);
 };
 
 var togglePlayFromPlayerBar = function() {
